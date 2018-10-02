749efbede476ee0a8561a068b461eb29aa0e913d
Volodymyr Nakutnyi <volodymyrnakutnyi@gmail.com>
2018-10-02 23:26:59 +0300

    another part

--- FL_9_3_homework_oop/homework/js/app.js
+++ FL_9_3_homework_oop/homework/js/app.js
@@ -2,139 +2,137 @@
 function Product (name, description, price ){
 this.name=name;
 this.description=description;
-let _price=price;
 let historyLog=[];
-let cardNameAdd='';
+this.cartDate;
+this.cartPlace='';
+
 this.getPrice=function(){
-    return _price;
+    return price;
 }
 this.setPrice=function(newPriceValue){
-    if(newPriceValue>_price){
-        historyLog.push( `Change price fron ${_price} to ${newPriceValue} `);
-        _price=newPriceValue;
+    if(newPriceValue>price){
+        historyLog.push( `Change price fron ${price} to ${newPriceValue} `);
+        price=newPriceValue;
     }else{
-        historyLog.push( `Try change value from ${_price} to ${newPriceValue}`);
+        historyLog.push( `Try change value from ${price} to ${newPriceValue}`);
     }
     return this;
 }
 this.add=function(value){
-    cardNameAdd=value;
-    historyLog.push(`${this.name} is added to ${cardNameAdd} on ${new Date()}. `);
+    this.cartPlace=value;
+    historyLog.push(`${this.name} is added to ${this.cartPlace} on ${this.cartDate}. `);
     return this;
 }
-this.remove=function(value){
-    cardNameAdd='';
-    historyLog.push(` ${this.name} is remove from ${value} on ${new Date()} `);
+this.removeProduct=function(value){
+    this.cartPlace='';
+    historyLog.push(` ${this.name} is remove from ${value} on ${getDatecreate()} `);
     return this;
 }
 this.getHistory=function(){
     historyLog.forEach((elem) => console.log(elem));
 }
 }
-
+ function getDatecreate(){
+     return new Date();
+ }
 
 function ShoppingCart(name, owner, maxCount){
 this.name=name;
-this.owner=owner;
-this.maxCount=maxCount;
 let history=[];
 let productArray=[];
-let dateOdAddingToCart=[];
-history.push(`${this.name} was created ${new Date()}, by ${this.owner}`);
+history.push(`${this.name} was created ${new Date()}, by ${owner}`);
 
-this.addNewProduct=function(value){
-if(value instanceof Product){
-if(productArray.length!==this.maxCount){
-    productArray.push(value);
-    history.push(` ${value.name} was added to ${this.name} on ${new Date()} `);
-}else{
-    let minPriceValue=productArray[0].getPrice();
-    let index=0;
-    for (let i = 0; i < productArray.length; i++){
-        if(productArray[i].getPrice()<minPriceValue){
-            minPriceValue=productArray[i].getPrice();
-            index=i;
+this.addNewProduct = function(value){
+    if(value instanceof Product){
+        if(!value.cartPlace){
+            if(productArray[maxCount-1]){
+                value.cartDate=getDatecreate();
+                productArray.push(value);
+                history.push(`${value.name} was added to ${this.name} on ${getDatecreate()} `);
+                
+                
+            }else{
+                let minPrice = productArray[0].getPrice();
+                let position = 0;
+    
+                for (let i = 0; i < productArray.length; i++) {
+                    if (productArray[i].getPrice() < minPrice) {
+                        minPrice = productArray[i].getPrice();
+                        position = i;
+            }
         }
+        history.push(`${productArray[position].name} removed from ${this.name} on ${new Date()}`);
+        productArray[position].removeProduct(this.name);
+        productArray.splice(position,1);
+        value.cartDate=getDatecreate();
+        history.push(`${value.name} was added to ${this.name} on ${value.cartDate}`);
+        productArray.push(value);
     }
-    productArray.splice(index,1);
-    productArray.push(value);
-   
-}
-value.add(this.name);
-history.push(`${value.name} was added to  ${this.name} on ${new Date()} `);
-
-}else{
-    console.log(` Bad value`);
-}
-return this;
-}
-this.removeProduct=function(id){
-    if(id>productArray.length){
-        console.warn(`ID is incorect.... try again`);
+    value.add(this.name);
+    } else if(value.cartPlace===this.name){
+        this.removeProduct(value);
+        this.addNewProduct(value)
+    }else{
+        console.log(`${value.name} do not use...`)
     }
-        dateOdAddingToCart.push(new Date());
-        // history.push(` ${productArray[id-1].name} was removed from ${ this.name } on ${new Date()} `);
-        // productArray[id-1].removeProduct(this.name);
-        productArray.slice(id-1,1);
-        return this;
+    } else {
+        console.log(`Pleace try again...`)
+    }
+    return this;
+    }
+this.removeProduct=function(value){
+   if(value instanceof Product){
+       let index=productArray.indexOf(value);
+       history.push(`${productArray[index].name} was delete from ${this.name} on ${new Date()}`);
+       productArray[index].removeProduct(this.name);
+       productArray.splice(index,1);
+   }
+   return this;
     
 }
 this.getAveragePrice =function(){
-    let average=0;
-    productArray.forEach(price => {
-        average+=price.getPrice();
-    });
-    return average/productArray.length;
+    return this.getTotalPrice()/productArray.length;
 
+}
+this.getTotalPrice =function(){
+    let sumTotalPrice=0;
+    for (let i = 0; i < productArray.length; i++) {
+        sumTotalPrice += productArray[i].getPrice();
+    }
+    return sumTotalPrice;
 }
 this.getProducts=function(){
     return productArray;
 }
 this.getFormattedListOfProducts=function(){
-    return productArray.forEach(form => console.log(`${form.name} - is ${this.name} from
-    ${productArray.indexOf(form)}.Detailed product description: ${form.description} `));
+    return this.getProducts().map(val =>
+        `${val.name} - is on ${this.name} from ${val.cartDate}, \
+        description: ${val.description} `); 
+   
 }
-this.getTotalPrice =function(){
-    let sumTotalPrice=0;
-    productArray.forEach(elem => {
-sumTotalPrice+=elem.getPrice();
-    });
-    return sumTotalPrice;
-}
+
 this.getHistory = function(){
     history.forEach(log => console.log(log));
 }
 }
-const banana = new Product({
-    name: 'banana',
-    description: {
-      color: 'yellow',
-      size: 'medium'
-    },
-    price: 45
-  });
-  
-  const apple = new Product({
-    name: 'apple',
-    description: {
-      color: 'red',
-      size: 'small'
-    },
-    price: 30
-  });
-  
-  const stevesShopCart = new ShoppingCart({
-    name: 'stevesCart',
-    owner: 'Steve',
-    maxSize: 5
-  });
-  
-  stevesShopCart
-    .addNewProduct(banana)
-    .addNewProduct(banana)
-    .addNewProduct(apple)
-    .removeProduct(banana)
-   
-  
-  
-  
+////////////////
+let brendonShopCart = new ShoppingCart('Brenda', 'Brenda', 5);
+
+let banana = new Product('banana', {
+    color: 'yellow',
+    size: 'medium'
+}, 22);
+let apple = new Product('apple', {
+    color: 'red',
+    size: 'medium'
+}, 22);
+let avokado = new Product('avokado', {
+    color: 'green',
+    size: 'big'
+}, 22);
+
+
+brendonShopCart.addNewProduct(banana).addNewProduct(avokado).addNewProduct(apple);
+brendonShopCart.history();
+
+
